042ddc0f9c240744b4244e6f43464680a285c437
Alexander Bias <abias@ssystems.de>
2025-05-26 15:31:08 +0200

    Release v4.5-r1

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_guestredirect';
-$plugin->version = 2022112800;
-$plugin->release = 'v4.1-r1';
-$plugin->requires = 2022112800;
-$plugin->supported = [401, 401];
+$plugin->version = 2024100700;
+$plugin->release = 'v4.5-r1';
+$plugin->requires = 2024100700;
+$plugin->supported = [405, 405];
 $plugin->maturity = MATURITY_STABLE;
